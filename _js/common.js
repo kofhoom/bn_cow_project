b088ee1018a98c50d6854b82ecb53a6c5b37bdc3
masterJunsu <kofhoom@gmail.com>
2023-05-30 00:46:27 +0900

    [update] 페이지 생성
    [update] 페이지 생성

--- _js/common.js
+++ _js/common.js
@@ -1,38 +1,38 @@
 function chooseList(tarGetEl, type) {
   console.log({
     tarGetEl,
-    type
-  })
+    type,
+  });
 
   const tarGetElList = tarGetEl;
-  typeof tarGetElList === 'object' ?
-    true :
-    console.error('오브젝트 타입이 아닙니다.');
+  typeof tarGetElList === "object"
+    ? true
+    : console.error("오브젝트 타입이 아닙니다.");
 
-  tarGetElList.forEach(element => {
-    element.addEventListener('click', () => {
+  tarGetElList.forEach((element) => {
+    element.addEventListener("click", () => {
       for (let i = 0; i < tarGetEl.length; i++) {
-        tarGetEl[i].classList.remove('active')
+        tarGetEl[i].classList.remove("active");
       }
-      element.classList.add('active')
-    })
+      element.classList.add("active");
+    });
   });
 }
-
 
 function calRender(data = []) {
   // 데이터 가공
   const calendarList = data.reduce(
-    (acc, v) =>
-    ({
+    (acc, v) => ({
       ...acc,
-      [v.date]: [...(acc[v.date] || []), v.content]
-    }), {}
+      [v.date]: [...(acc[v.date] || []), v.content],
+    }),
+    {}
   );
+  console.log(calendarList);
   // pad method
   Number.prototype.pad = function () {
-    return this > 9 ? this : '0' + this;
-  }
+    return this > 9 ? this : "0" + this;
+  };
 
   // 달력 생성
   const makeCalendar = (date) => {
@@ -48,7 +48,7 @@ function calRender(data = []) {
     // 남은 박스만큼 다음달 날짜 표시
     const limitDay = firstDay + lastDay;
     const nextDay = Math.ceil(limitDay / 7) * 7;
-    let htmlDummy = '';
+    let htmlDummy = "";
 
     // 한달전 날짜 표시하기
     for (let i = 0; i < firstDay; i++) {
@@ -57,15 +57,15 @@ function calRender(data = []) {
 
     // 이번달 날짜 표시하기
     for (let i = 1; i <= lastDay; i++) {
-      const date = `${currentYear}-${currentMonth.pad()}-${i.pad()}`
+      const date = `${currentYear}-${currentMonth.pad()}-${i.pad()}`;
 
       htmlDummy += `
   <div class='
-      ${calendarList[date] ? 'calendar-active-style' : ''}' 
-      ${calendarList[date] ? "onclick=modalState('block')" : ''}>
+      ${calendarList[date] ? "calendar-active-style" : ""}' 
+      ${calendarList[date] ? "onclick=modalState('block')" : ""}>
     <span>${i}</span>
     <p>
-      ${calendarList[date]?.join("</p><p>") || ''}
+      ${calendarList[date]?.join("</p><p>") || ""}
     </p>
   </div>
 `;
@@ -77,27 +77,24 @@ function calRender(data = []) {
     }
 
     document.querySelector(`.dateBoard`).innerHTML = htmlDummy;
-    document.querySelector(`.dateTitle`).innerText = `${currentYear}년 ${currentMonth}월`;
-  }
+    document.querySelector(
+      `.dateTitle`
+    ).innerText = `${currentYear}년 ${currentMonth}월`;
+  };
 
-  const date = new Date('2023-05-15');
+  const date = new Date("2023-05-15");
 
   makeCalendar(date);
 
   // 이전달 이동
   document.querySelector(`.prevDay`).onclick = () => {
     makeCalendar(new Date(date.setMonth(date.getMonth() - 1)));
-  }
+  };
 
   // 다음달 이동
   document.querySelector(`.nextDay`).onclick = () => {
     makeCalendar(new Date(date.setMonth(date.getMonth() + 1)));
-  }
+  };
 }
 
-
-
-export {
-  chooseList,
-  calRender
-}
+export { chooseList, calRender };
